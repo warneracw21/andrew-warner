237adf22df12ddb5bceb9588d109dc61cf7cd57b
Andrew Warner <warneracw@gmail.com>
2020-03-04 09:21:54 -0500

    Updates

---
+++ precache-manifest.60127d1dade6b6ad85a93aa1b5c24b2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df095a0939a4b39f0e829017c8e7326",
+    "url": "/andrew-warner/index.html"
+  },
+  {
+    "revision": "c5e15cf17de5aa638a30",
+    "url": "/andrew-warner/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "41853c5367f62b40125e",
+    "url": "/andrew-warner/static/js/2.3e6c2a6d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/andrew-warner/static/js/2.3e6c2a6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5e15cf17de5aa638a30",
+    "url": "/andrew-warner/static/js/main.f96833b7.chunk.js"
+  },
+  {
+    "revision": "7f79f5e5c8bfe50007f5",
+    "url": "/andrew-warner/static/js/runtime-main.934d9aa2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/andrew-warner/static/media/logo.5d5d9eef.svg"
+  }
+]);
